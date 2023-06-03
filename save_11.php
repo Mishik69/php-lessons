680aa27013a2add72a07a5322fce31e976f6b76f
Mishik69 <133650039+Mishik69@users.noreply.github.com>
2023-06-04 01:47:43 +0300

    Add files via upload

---
+++ save_11.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+
+$text=$_POST['text'];
+$pdo = new PDO('mysql:host=localhost;dbname=my_table;','root','');
+
+$sql = "SELECT *FROM my_table WHERE text=:text";
+$statement= $pdo->prepare($sql);
+$statement->execute(['text'=>$text]);
+$task=$statement->fetch(PDO::FETCH_ASSOC);
+
+if (!empty($task)){
+    $message = "Введенная запись уже присутвует в базе.";
+    $_SESSION['danger']= $message;
+    header("location: test11.php");
+    exit;
+}
+
+
+
+$sql= 'INSERT INTO my_table (text) VALUES (:text)';
+$statement= $pdo->prepare($sql);
+$statement->execute(['text'=> $text]);
+
+$message = "Введенная запись успешно добавлена в базу.";
+$_SESSION['success']= $message;
+
+header("location: test11.php");
+?>
